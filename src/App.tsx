96ec56a4681341c2ac8b51c29763ab7f7e0eda2d
Muhammad Muzammil <yoursmuzammil16070@gmail.com>
2025-07-15 16:38:43 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Classes from "./pages/Classes";
@@ -22,21 +22,24 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
-        <div className="min-h-screen bg-gradient-to-br from-blue-50 to-green-50">
+        <div className="min-h-screen flex flex-col bg-gradient-to-br from-blue-50 to-green-50">
           <Navbar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/classes" element={<Classes />} />
-            <Route path="/teachers" element={<Teachers />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/feedback" element={<Feedback />} />
-            <Route path="/about" element={<About />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
+
+          <div className="flex-grow">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/classes" element={<Classes />} />
+              <Route path="/teachers" element={<Teachers />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/feedback" element={<Feedback />} />
+              <Route path="/about" element={<About />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </div>
+
+          <AnimatedFooter /> {/* ✅ Always visible footer */}
         </div>
       </BrowserRouter>
-      {/* Animated Footer */}
-      <AnimatedFooter />
     </TooltipProvider>
   </QueryClientProvider>
 );
